ab0d047aad3a2cd70ddc537ff340d9ac2d425ab5
Piklar <ernzmanalo2003@gmail.com>
2025-05-15 09:44:07 +0800

    Changed directory from api to manalo

--- post-api/src/main/java/com/manalo/post/PostController.java
+++ post-api/src/main/java/com/manalo/post/PostController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/manalo/posts")
 public class PostController {
 
     @Autowired
